31142b365af37b085e2c7d1f5290aacd42ca1713
SahilZakaria <aunik849@gmail.com>
2025-04-14 22:06:58 -0400

    Update event_manager.cpp

---
+++ source/event/event_manager.cpp
@@ -0,0 +1,15 @@
+#include "event_manager.h"
+
+void EventManager::registerListener(EventType eventType, std::function<void(const EventData&)> callback) {
+
+    listeners[eventType].push_back(callback);
+}
+
+void EventManager::notify(EventType eventType, const EventData &data) {
+
+    if (listeners.find(eventType) != listeners.end()) {
+        for (auto &callback : listeners[eventType]) {
+            callback(data);
+        }
+    }
+}
