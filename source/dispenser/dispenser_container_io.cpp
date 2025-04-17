15724916cdd4e135b933aebf3b87f3374748dede
DZI-dotWRLD <danila.ishchanka@gmail.com>
2025-04-16 21:35:02 -0400

    Update dispenser_container_io.cpp

---
+++ source/dispenser/dispenser_container_io.cpp
@@ -0,0 +1,37 @@
+#include "DispenserContainerIO.h"
+#include <iostream>
+
+class ConsoleDispenserContainerIO : public DispenserContainerIO {
+public:
+    void displayMenu() override {
+        std::cout << "==== VENDING MACHINE MENU ====\n";
+        std::cout << "1. Select Beverage\n";
+        std::cout << "2. Maintenance Mode\n";
+        std::cout << "3. Exit\n";
+    }
+
+    void handleSelectionInput() override {
+        std::cout << "Please enter your selection (1-3): ";
+        int selection;
+        std::cin >> selection;
+        std::cout << "You selected option " << selection << ".\n";
+    }
+
+    void displaySelectedBeverage() override {
+        std::cout << "Your beverage has been selected.\n";
+    }
+
+    void displayDispensedBeverage() override {
+        std::cout << "Dispensing your beverage. Please wait...\n";
+    }
+
+    void displaySlotValues() override {
+        std::cout << "Displaying all slot values...\n";
+        // loop over slots here if connected to a container
+    }
+
+    void inputRefillBeverages() override {
+        std::cout << "Enter new beverage details to refill the slots.\n";
+        // refill logic
+    }
+};
