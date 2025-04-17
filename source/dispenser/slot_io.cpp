b01d5159eade054390d8bc0d473d7cb1b0777bc8
DZI-dotWRLD <danila.ishchanka@gmail.com>
2025-04-16 21:26:50 -0400

    Update slot_io.cpp

---
+++ source/dispenser/slot_io.cpp
@@ -0,0 +1,13 @@
+#include "SlotIO.h"
+
+void SlotIO::displaySlot(const Slot& slot) {
+    std::cout << "Slot ID: " << slot.getID() << std::endl;
+    std::cout << "Price: $" << slot.getPrice() << std::endl;
+    std::cout << "State: " << slot.getState() << std::endl;
+    std::cout << "Beverages in slot: " << slot.getCurrentCount() << std::endl;
+
+    std::vector<Beverage> bevs = slot.getBeverages();
+    for (const auto& b : bevs) {
+        std::cout << "- " << b.getName() << std::endl;
+    }
+}
