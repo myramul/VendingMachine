8e72723216fb80fb12b6a9f002c67e506106c3ae
DZI-dotWRLD <danila.ishchanka@gmail.com>
2025-04-16 21:16:25 -0400

    Update beverage.h

--- header/dispenser/beverage.h
+++ header/dispenser/beverage.h
@@ -1,20 +1,19 @@
 #ifndef BEVERAGE_H
 #define BEVERAGE_H
+
 #include <string>
 
-namespace using std;
-
-
 class Beverage {
+private:
+    std::string beverageName;
 
-private: string beverageName;
+public:
+    Beverage();  // default constructor
+    Beverage(const std::string& name);  // constructor with name
 
-public :
-	Beverage(); //default 
-	Beverage(const string name); // should we pass string as a reference???? or leave it like tbis??????
-	void setName(string name);// should we pass string as a reference???? or leave it like tbis??????
-	string getName() const;
-
+    void setName(const std::string& name);
+    std::string getName() const;
 };
 
 #endif // BEVERAGE_H
+
