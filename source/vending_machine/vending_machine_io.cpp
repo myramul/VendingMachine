85e3cb16669d43c3b219d7ca0aa8295673955d4e
Nodira Kazakova <kazakovanodira04@gmail.com>
2025-04-28 09:35:51 -0400

    Update vending_machine_io.cpp

--- source/vending_machine/vending_machine_io.cpp
+++ source/vending_machine/vending_machine_io.cpp
@@ -1,13 +1,32 @@
 #include "vending_machine/vending_machine_io.h"
+#include "vending_machine/vending_machine.h"
 #include <iostream>
+#include <string>
+
+VendingMachineIO::VendingMachineIO(VendingMachine* machine)
+    : vendingMachine(machine) {}
 
 void VendingMachineIO::displayWelcomeMessage() {
     std::cout << "Welcome to Bug Busters Vending Machine!\n";
-    // it should also say the coins it accepts and show the menu
-    // it should be on stanby for input. maybe have a message press enter to continue
-    // if the maintennace person types the passord it goes to maintennace mode
-    // if the user types in enter it then tells the event manager to tell the coin slot to start accepting coins
-    // by calling the function enterProcessingMode()
+    std::cout << "We accept: Nickels ($0.05), Dimes ($0.10) and Quarters ($0.25).\n";
+    std::cout << "Available Beverages:\n";
+    // for now:
+    std::cout << "1. Cola - $1.00\n";
+    std::cout << "2. Water - $0.75\n";
+    std::cout << "3. Juice - $1.25\n";
+    std::cout << "Press [Enter] to continue or type maintenance password:\n";
+
+    std::string input;
+    std::getline(std::cin, input);
+
+    if (vendingMachine->authenticateMaintenancePasscode(input)) {
+        vendingMachine->enterMaintenanceMode();
+        displayMaintenanceMenu();
+        handleMaintenanceMenuSelection();
+    } else {
+        std::cout << "Starting normal transaction...\n";
+        vendingMachine->enterProcessingMode();
+    }
 }
 
 void VendingMachineIO::displayMaintenanceMenu() {
@@ -20,12 +39,32 @@ void VendingMachineIO::displayMaintenanceMenu() {
 }
 
 void VendingMachineIO::handleMaintenanceMenuSelection() {
-    // for each selection it should call the appropriate functions in the involved components
-    // or maybe add a new event and then call the event manager and then it will call the appropriate functions
-    // the second option might be easier
-    std::cout << "Enter an option number: ";
-    int choice;
-    std::cin >> choice;
-    // For this prototype, we'll just echo the selection
-    std::cout << "You selected option " << choice << ".\n";
+    int choice = 0;
+    do {
+        std::cout << "\nEnter an option number: ";
+        std::cin >> choice;
+        std::cin.ignore(); // Clear the buffer after reading int input
+
+        switch (choice) {
+            case 1:
+                vendingMachine->collectMoney();
+                break;
+            case 2:
+                vendingMachine->refillChange();
+                break;
+            case 3:
+                vendingMachine->refillBeverages();
+                break;
+            case 4:
+                vendingMachine->viewReports();
+                break;
+            case 5:
+                vendingMachine->lockMachine();
+                vendingMachine->enterIdleMode();
+                std::cout << "Exiting maintenance mode.\n";
+                break;
+            default:
+                std::cout << "Invalid option. Please try again.\n";
+        }
+    } while (choice != 5);
 }
