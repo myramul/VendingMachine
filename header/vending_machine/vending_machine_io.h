1e951aaac03d9f900838e5a0e3f6ddeea58b41eb
Nodira Kazakova <kazakovanodira04@gmail.com>
2025-04-30 21:57:17 -0400

    Update vending_machine_io.h

--- header/vending_machine/vending_machine_io.h
+++ header/vending_machine/vending_machine_io.h
@@ -1,8 +1,15 @@
 #ifndef VENDING_MACHINE_IO_H
 #define VENDING_MACHINE_IO_H
 
+class VendingMachine; // Forward declare
+
 class VendingMachineIO {
+private:
+    VendingMachine* vendingMachine;
+
 public:
+    VendingMachineIO(VendingMachine* machine);
+
     void displayWelcomeMessage();
     void displayMaintenanceMenu();
     void handleMaintenanceMenuSelection();
